b3245e909ce296240c904fc149ff076fe13e3c9e
Doug Walter <dougwritescode@gmail.com>
2017-02-03 18:16:28 -0500

    update of git repo name

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
 	description = 'A generator of xkcd-style passwords',
 	author = 'Doug Walter',
 	author_email = 'dougwritescode@gmail.com',
-	url = 'https://github.com/dougwritescode/xkcdpass',
-	download_url = 'https://github.com/dougwritescode/xkcdpass/tarball/0.1', 
+	url = 'https://github.com/dougwritescode/xkcdpassword',
+	download_url = 'https://github.com/dougwritescode/xkcdpassword/tarball/0.1', 
 	keywords = ['python', 'xkcd', 'utilities', 'security'], 
 	classifiers = [],
 )
