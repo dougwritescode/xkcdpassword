a06c26dd79d7ebbdd1d28c6aa74936e3e1d68d8e
ale-rt <alessandro.pisa@gmail.com>
2017-03-13 00:11:26 +0100

    pep8

--- setup.py
+++ setup.py
@@ -2,32 +2,35 @@
 
 from setuptools import setup
 
+
 install_requires = ['pyperclip']
 
 setup(
-	name = 'xkcdpassword',
-	packages = ['xkcdpassword'],
-	version = '0.6.5',
-	description = 'A generator of xkcd-style passwords',
-	author = 'Doug Walter',
-	author_email = 'dougwritescode@gmail.com',
-	url = 'https://github.com/dougwritescode/xkcdpassword',
-	download_url = 'https://github.com/dougwritescode/xkcdpassword/tarball/0.6.5', 
-	keywords = 'python xkcd utilities security password', 
-	license='MIT',
-	classifiers = ['Development Status :: 3 - Alpha',
-		'Environment :: Console',
-		'License :: OSI Approved :: MIT License',
-		'Natural Language :: English',
-		'Programming Language :: Python :: 2.7',
-		'Topic :: Security',
-		'Topic :: Utilities',],
-	entry_points = {
-		'console_scripts': [
+    name='xkcdpassword',
+    packages=['xkcdpassword'],
+    version='0.6.5',
+    description='A generator of xkcd-style passwords',
+    author='Doug Walter',
+    author_email='dougwritescode@gmail.com',
+    url='https://github.com/dougwritescode/xkcdpassword',
+    download_url='https://github.com/dougwritescode/xkcdpassword/tarball/0.6.5',  # noqa
+    keywords='python xkcd utilities security password',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Security',
+        'Topic :: Utilities',
+    ],
+    entry_points={
+        'console_scripts': [
             'xkcdpassword = xkcdpassword.xkcdpassword:main',
         ],
         'gui_scripts': []
-	},
-	zip_safe=False,
+    },
+    zip_safe=False,
     include_package_data=True,
 )
