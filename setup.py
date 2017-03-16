67d4951276a8a5893e4859cabd2221b52b7b2e7a
Doug Walter <dougwritescode@gmail.com>
2017-03-15 21:08:32 -0400

    Setup.py adjustments; Thanks to ale-rt for catching bad install_requires.

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ install_requires = ['pyperclip']
 setup(
     name='xkcdpassword',
     packages=['xkcdpassword'],
-    version='0.6.5',
+    version='0.6.6',
     description='A generator of xkcd-style passwords',
     author='Doug Walter',
     author_email='dougwritescode@gmail.com',
     url='https://github.com/dougwritescode/xkcdpassword',
-    download_url='https://github.com/dougwritescode/xkcdpassword/tarball/0.6.5',  # noqa
+    download_url='https://github.com/dougwritescode/xkcdpassword/tarball/0.6.6',  # noqa
     keywords='python xkcd utilities security password',
     license='MIT',
     classifiers=[
